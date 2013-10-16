a062cfba50afad482718b395380c7764dc7f40ee
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-16 16:59:06 +0200

    * factorized code a bit, login validation is now done by fns/user_handler.php

--- index.php
+++ index.php
@@ -1,67 +1,47 @@
 <?php
     $page_name = 'Index Page';
     require_once('includes/header.php');
+    require_once('fns/user_handler.php');
+
+    $validator = new UserHandler();
+
+    if(isset($_GET['feedback']))
+    {
+        $feedback = ($_GET['feedback']);
+        $feedback = '<i class="error">'.$feedback.'</i>';
+    }
+    else
+    {
+        $feedback = '<i class="success">Please enter your log in details</i>';
+    }
+
+    if(($_SERVER['REQUEST_METHOD'] == 'POST'))
+    {
+        $validator->validate_login($_POST['user_name'], $_POST['user_password']);
+    }
+
 ?>
 
-    <div class="grid_4">
-        <h3>Sign in</h3>
-    </div>
+<div class="stalactite prefix_3 grid_6">
 
-    <div class="grid_8">
-        <?php
-            if(!empty($_POST['user_name']))
-            {
-                $user_name = $_POST['user_name'];
-                if($user_name == 'admin_bogosi')
-                {
-                    if(!empty($_POST['user_password']))
-                    {
-                        $pword = $_POST['user_password'];
-                        if($pword == 'KEmohae')
-                        {
-                            session_start();
-                            $_SESSION['admin_active'] = 'admin_bogosi';
-                            header('Location: open_file.php');
-                        }
-                        else
-                        {
-                            echo 'Incorrect login details!';
-                        }
-                    }
-                    else
-                    {
-                        echo 'Please enter password';
-                    }
-                }
-                else
-                {
-                    echo 'Incorrect login details!';
-                }
-            }
-            else
-            {
-                echo 'Please enter your details';
-            }
-        ?>
-    </div>
+    <h3>Sign in</h3>
 
+    <p><?php echo $feedback; ?></p>
 
-    <div class="grid_8">
-        <p></p>
-        <form method="post" action="index.php">
-            <p>
-                User name:
-                <br>
-                <input type="text" name="user_name" width="80" placeholder="user name" value="<?php echo $user_name; ?>">
-            </p>
-            <p>
-                Password:
-                <br>
-                <input type="password" name="user_password" width="80" placeholder="password">
-            </p>
+    <form id="auth-form" method="post" action="index.php">
+        <p>
+            User name:
+            <br>
+            <input type="text" name="user_name" width="80" placeholder="user name" value="<?php echo $_POST['user_name']; ?>">
+        </p>
+        <p>
+            Password:
+            <br>
+            <input type="password" name="user_password" width="80" placeholder="password">
+        </p>
 
-            <input type="submit" value="Sign in">
-        </form>
-    </div>
+        <input class="open_btn" type="submit" value="Sign in">
+    </form>
+</div>
 
-<?php require_once('includes/footer.php')?>
+<?php require_once('includes/footer.php');?>
