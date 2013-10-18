f0b982fd45766910e1bdae6e802d731b303b1fd6
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-18 09:10:06 +0200

    * defined paths to the fundamental pages of the site, this file currently isn't integrated into into the system as yet

--- includes/nav_links.php
+++ includes/nav_links.php
@@ -1,15 +1,18 @@
 <?php
 
+    # Navigation links
     define('nav_login', realpath('../index.php'));
-    define('', '');
-    define('', '');
-    define('', '');
-    define('', '');
-    define('', '');
-    define('', '');
-    define('', '');
-    define('', '');
-    define('', '');
+    define('nav_open_file', realpath('../open_file.php'));
+    define('nav_file_explore', realpath('../file_explore.php'));
+    define('nav_shell', realpath('../shell_command.php'));
+
+    #
+    define('', realpath(''));
+    define('', realpath(''));
+    define('', realpath(''));
+    define('', realpath(''));
+    define('', realpath(''));
+
 
     var_dump(nav_login);
 
