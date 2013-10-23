3b4997227f0f51885bc624e2c983e4dd0cee6fca
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-23 13:20:59 +0200

    * removed comments within the script

--- sign_out.php
+++ sign_out.php
@@ -1,15 +1,6 @@
 <?php
-//# start session
+# start session
 session_start();
-//
-//# unset session variables
-//unset($_SESSION['admin_active']);
-//
-//#destroy session
-//session_destroy();
-//
-//# redirect user back to index page
-//header('Location: index.php');
 
 require_once('fns/user_handler.php');
 $user_handler = new UserHandler();
