6ac3987b627bf6bce4aef16c1827ee5e9aa92653
Pride Mokhele <pride.mokhele@gmail.com>
2013-10-23 13:13:34 +0200

    * now uses the logout function within the UserHandler class

--- sign_out.php
+++ sign_out.php
@@ -1,13 +1,18 @@
 <?php
-# start session
+//# start session
 session_start();
+//
+//# unset session variables
+//unset($_SESSION['admin_active']);
+//
+//#destroy session
+//session_destroy();
+//
+//# redirect user back to index page
+//header('Location: index.php');
 
-# unset session variables
-unset($_SESSION['admin_active']);
+require_once('fns/user_handler.php');
+$user_handler = new UserHandler();
+$user_handler->logout();
 
-#destroy session
-session_destroy();
-
-# redirect user back to index page
-header('Location: index.php');
 ?>
